e9d90821ec865f3ef6961e47dc9e55af90fcebe1
Evgeny Koltsov <ekoltsov@gmail.com>
2025-01-21 20:51:28 +0300

    change debugger hotkeys to match VS Code order hotkeys

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -90,11 +90,12 @@ return {
       end)
 
       vim.keymap.set('n', '<F1>', dap.continue)
-      vim.keymap.set('n', '<F2>', dap.step_into)
-      vim.keymap.set('n', '<F3>', dap.step_over)
+      vim.keymap.set('n', '<F2>', dap.step_over)
+      vim.keymap.set('n', '<F3>', dap.step_into)
       vim.keymap.set('n', '<F4>', dap.step_out)
       vim.keymap.set('n', '<F5>', dap.step_back)
       vim.keymap.set('n', '<F13>', dap.restart)
+      vim.keymap.set('n', '<F9>', dap.toggle_breakpoint)
 
       dap.listeners.before.attach.dapui_config = function()
         ui.open()
